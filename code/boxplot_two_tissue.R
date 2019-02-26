87f37e940b4d42bd8e1951d3c2541a70cd3fde88
lfiedler <lindsey@lindsey-ideapad.com>
2019-02-26 17:33:15 -0500

    Added R and Python code

---
+++ code/boxplot_two_tissue.R
@@ -0,0 +1,126 @@
+
+
+boxplotForSampling <- function(x,sam.num = 10,
+                               lableName = 'gene expression',
+                               col = 'forestgreen')
+{
+  idNeed <- sample(1:ncol(x), sam.num)
+  dfDraw <- x[,idNeed]
+  #print(colnames(drawDf))
+  
+  plot(x=NA,
+       y=NA,
+       ylim = c(1-0.5, sam.num+0.5),
+       xlim = c(min(dfDraw)-min(dfDraw)/10, max(dfDraw)+max(dfDraw)/10), 
+       yaxt= "n",
+       #xaxt = "n",
+       xlab = lableName, 
+       ylab = '',#bty = 'n',
+       cex.lab=1.2, cex.axis=1.2, cex.main=1, cex.sub=1)  
+  axis(2, at = 1:sam.num, 
+       labels = colnames(dfDraw), las = 1,
+       cex.axis=0.8)
+  boxplot(x[,idNeed], horizontal = T, add = T,
+          xaxt = "n", yaxt = "n",
+          outline = F, col = col,medcol = 'gray90')
+}
+
+diffRun <- function(df = dfAll, sample.num = 10,
+                    twoTissueCandidate = c('Brain', 'Blood'),
+                    pickGene = 0.05)
+{
+    id1     <- grep(twoTissueCandidate[1], colnames(df))
+    id2     <- grep(twoTissueCandidate[2], colnames(df))
+  
+    idUse1  <- sample(id1, sample.num)
+    idUse2  <- sample(id2, sample.num)
+
+    df1 <- df[,idUse1]
+    df2 <- df[,idUse2]
+    #print(head(df2))
+    df1mean <- apply(df1, 1, mean)
+    df2mean <- apply(df2, 1, mean)
+    
+    foldChange <- df2mean/(df1mean + min(df1mean[which(df1mean != 0)]))
+    pValue     <- sapply(1:nrow(df),
+                        function(x){test <- wilcox.test(df1[x,], 
+                                                        df2[x,]);
+                                    return(test$p.value)})
+    uperGene   <- which(foldChange > 3 & pValue < 1e-3)
+    downGene   <- which(foldChange < 1/3 & pValue < 1e-3)
+    #print(length(uperGene))
+    #idDecreasing <- order(foldChange, decreasing = T)
+    #num.Gene     <- floor(pickGene * nrow(dfAll))
+    
+    #uperGene <- idDecreasing[1:num.Gene]
+    #downGene <- idDecreasing[(nrow(df) - num.Gene):nrow(df)]
+    
+    plot(log(df1mean + 1)[-c(uperGene, downGene)], 
+         log(df2mean + 1)[-c(uperGene, downGene)],
+         xlab = twoTissueCandidate[1],
+         ylab = twoTissueCandidate[2],
+         #yaxt= "n",
+         #xaxt = "n",
+         las = 1,
+         pch = 19, cex = 0.5, col = 'gray80'
+         )
+    points(log(df1mean + 1)[uperGene], 
+           log(df2mean + 1)[uperGene],
+           pch = 21, bg = 'forest green', cex = 0.8)
+    points(log(df1mean + 1)[downGene], 
+           log(df2mean + 1)[downGene],
+           pch = 21, bg = 'red', cex = 0.8)
+    
+    return(list(upRegulatedGene   = rownames(df)[uperGene],
+           downRegulatedGene = rownames(df)[downGene]))
+    
+}
+
+gm_mean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
+sizeFactCal <- function(x)
+{
+  geM <- apply(x, 1, gm_mean)
+  sizeFactor <- apply(x/geM,2,function(x){median(x, na.rm = T)})
+  sizeFactor[sizeFactor == 0] <- 1
+  nor_X <- sapply(1:ncol(x), function(z){x[,z]/sizeFactor[z]})
+  return(nor_X)
+}
+
+
+runningProgram <- function(data = dfAll,
+                           disData = diseaseAssociatedGene,
+                           tissueName = c('Brain', 'Blood'))
+{
+  #trim data based on diseaseAssociatedGene
+  iiiid <- match(unique(disData$Gene), data$ID2)
+  df    <- data[iiiid[!is.na(iiiid)], ]
+  df    <- dfAll[which(rowSums(df[,3:ncol(df)]) > ncol(df)), ]
+  
+  df_nor           <- sizeFactCal(df[,3:ncol(df)])
+  colnames(df_nor) <- colnames(df)[3:ncol(df)]
+  rownames(df_nor) <- rownames(df)
+  print(head(df_nor[,1:10]))
+  aa <- diffRun(df_nor, 
+                twoTissueCandidate = tissueName)
+}
+
+
+
+
+##load trimmed tissue data
+dfAll <- read.table('aa',header = T, sep = '\t')
+##load disease associated genes
+diseaseAssociatedGene <- read.delim2('PheGenI_Association_full.tab',
+                                     header = T, sep = '\t')
+runningProgram(dfAll, diseaseAssociatedGene,
+               tissueName = c('Brain', 'Blood'))
+# #pdf('boxplot_deseq.pdf',height = 5, width = 4)
+# par(mar =c(4,10,1,1))
+# boxplotForSampling(log(nor_dataCombine +1),
+#                    lableName = 'deseq_normalization',
+#                    sam.num = 20)
+# #dev.off()
+# 
---
+++ code/data.generate.py
@@ -0,0 +1,73 @@
+# -*- coding: UTF-8 -*-
+import sys
+import subprocess
+import os
+import numpy
+import random
+from operator import itemgetter
+import math
+
+class rnaSeqData:
+    def __init__(self, rnaData, descriptionData, outputFile, requiredTissue):
+        self.rna = rnaData
+        self.description = descriptionData
+        self.output      = outputFile
+        self.queryTissue = requiredTissue
+    
+    def descriptHash(self):
+        des_hash = {}
+        with open(self.description, 'r') as f:
+            line = f.readline()
+            while line:
+                [id, tissue] = itemgetter(0,5)(line.split('\t'))
+                des_hash[id] = tissue
+                line = f.readline()
+        return(des_hash)
+
+    def columnExtract(self):
+        
+        tissueHash = self.descriptHash()
+        
+        with open (self.rna, 'r') as f:
+            topLine = f.readline()
+        
+        neededColumnList =[0,1]
+        neededColumnName =['ID1','ID2']
+        neededColumn = 0
+        list_id = topLine.split('\t')
+        for i in list_id:
+            neededColumn = neededColumn + 1
+            if (i in tissueHash and tissueHash[i] in requiredTissue):
+                neededColumnList.append(neededColumn)
+                neededColumnName.append('_'.join([tissueHash[i], str(neededColumn)]))
+
+        #print(neededColumnList)
+        #print(neededColumnName)
+        
+        jjj = 0
+        with open(self.output, 'w') as fw:
+            fw.write('\t'.join(neededColumnName))
+            fw.write('\n')
+            with open(self.rna,'r') as f:
+                line = f.readline()
+                while line:
+                    jjj = jjj + 1
+                    if jjj > 1:
+                        line_list = line.split('\t')
+                        eleNeeded = [line_list[j] for j in neededColumnList]
+                        aa = '\t'.join(eleNeeded)
+                        
+                        fw.write(aa)
+                        fw.write('\n')
+                    line = f.readline()
+                
+            
+
+
+if __name__ == '__main__':
+    [rnaData, des, outputFile, *requiredTissue] = sys.argv[1:]
+    #print(requiredTissue)
+    class_go = rnaSeqData(rnaData, des, outputFile, requiredTissue)
+    class_go.columnExtract()
+    #aa = class_go.descriptHash()
+    #print(aa)
