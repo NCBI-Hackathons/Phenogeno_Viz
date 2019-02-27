7496cc6a8c7c079e52119a1dc4ac2a08df6b02fb
Guy Dayhoff <dayhoff@js-17-119.jetstream-cloud.org>
2019-02-26 22:08:50 -0600

    updated the run script

--- code/run.sh
+++ code/run.sh
@@ -1,2 +1,25 @@
-time python3 data.generate.py dataset.gct anno.txt OUTPUT 'Brain' 'Blood'
-time Rscript boxplot_two_tissue.R
+#validate argument count
+if [ "$#" -ne 4 ]; then
+  echo "Illegal number of parameters."
+  exit
+
+#validate dataset file
+elif [ ! -f $1 ]; then
+  echo "Dataset file (arg1) not found."
+  exit
+
+#validate annotation file
+elif [ ! -f $2 ]; then
+  echo "Annotation file (arg2) not found."
+  exit
+fi
+
+#python3 data.generate.py $1 $2 slow_anal.out \'$3\' \'$4\'
+
+#validate slow_anal.out was produced
+if [ ! -f 'slow_anal.out' ]; then
+  echo "The expected output from data.generate.py cannot be found."
+  exit
+fi
+
+Rscript boxplot_two_tissue.R $3 $4
